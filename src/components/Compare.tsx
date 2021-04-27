8a1de68cf9ac594fef7e713f5b364795cc0476c5
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-27 00:03:08 -0600

    series

--- src/components/Compare.tsx
+++ src/components/Compare.tsx
@@ -5,6 +5,70 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import StockService from '../services/stock';
 import StepSeries from './StepSeries'
 const { earnings } = StockService;
+
+const data = [
+  {
+    month: "Jan",
+    series2: 51,
+    series1: 125
+  },
+  {
+    month: "Feb",
+    series2: 91,
+    series1: 132
+  },
+  {
+    month: "Mar",
+    series2: 34,
+    series1: 141
+  },
+  {
+    month: "Apr",
+    series2: 47,
+    series1: 158
+  },
+  {
+    month: "May",
+    series2: 63,
+    series1: 133
+  },
+  {
+    month: "June",
+    series2: 58,
+    series1: 143
+  },
+  {
+    month: "July",
+    series2: 56,
+    series1: 176
+  },
+  {
+    month: "Aug",
+    series2: 77,
+    series1: 194
+  },
+  {
+    month: "Sep",
+    series2: 99,
+    series1: 115
+  },
+  {
+    month: "Oct",
+    series2: 106,
+    series1: 134
+  },
+  {
+    month: "Nov",
+    series2: 88,
+    series1: 110
+  },
+  {
+    month: "Dec",
+    series2: 56,
+    series1: 91
+  }
+];
+
 export default function() {
   const history = useHistory();
   const { symbols } = useParams<{symbols:string}>();
@@ -34,7 +98,7 @@ export default function() {
         ))}
       </ButtonGroup>
 
-      <StepSeries />
+      <StepSeries data={data}/>
     </div>
   )
 }
