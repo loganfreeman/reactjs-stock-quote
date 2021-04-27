9d38a36bd7ffbeb3d062b287893c757d2e85d2e7
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-26 23:51:59 -0600

    earnings

--- src/components/Compare.tsx
+++ src/components/Compare.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useHistory, useParams } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-
+import StockService from '../services/stock';
+const { earnings } = StockService;
 export default function() {
   const history = useHistory();
   const { symbols } = useParams<{symbols:string}>();
-
+  const [reportedEPS, setReportedEPS] = useState<any[]>();
+  useEffect(() => {
+    (async function() {
+      const stocks = symbols.split(",");
+      if(stocks.length>0) {
+        let earningsArray: any[] = [];
+        for(const stock of stocks) {
+          const {data} = await earnings(stock);
+          earningsArray.push(data);
+        }
+        setReportedEPS(earningsArray);
+      }
+    })();
+  }, [symbols]);
   return (
     <div>
       <p><Button variant="contained" onClick={e => history.push("/")}>Home</Button></p>
