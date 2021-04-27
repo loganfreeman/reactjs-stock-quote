024b439d15209ceb2bfdf91d40d593be1a7535ab
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-27 10:27:28 -0600

    use hash router

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorBoundary from './ErrorBoundary';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   useHistory
