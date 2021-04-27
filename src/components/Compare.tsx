664baa5439491fec2719a2e8b049bbbdf639fc71
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-27 00:39:18 -0600

    step series

--- src/components/Compare.tsx
+++ src/components/Compare.tsx
@@ -6,81 +6,27 @@ import StockService from '../services/stock';
 import StepSeries from './StepSeries'
 const { earnings } = StockService;
 
-const data = [
-  {
-    month: "Jan",
-    series2: 51,
-    series1: 125
-  },
-  {
-    month: "Feb",
-    series2: 91,
-    series1: 132
-  },
-  {
-    month: "Mar",
-    series2: 34,
-    series1: 141
-  },
-  {
-    month: "Apr",
-    series2: 47,
-    series1: 158
-  },
-  {
-    month: "May",
-    series2: 63,
-    series1: 133
-  },
-  {
-    month: "June",
-    series2: 58,
-    series1: 143
-  },
-  {
-    month: "July",
-    series2: 56,
-    series1: 176
-  },
-  {
-    month: "Aug",
-    series2: 77,
-    series1: 194
-  },
-  {
-    month: "Sep",
-    series2: 99,
-    series1: 115
-  },
-  {
-    month: "Oct",
-    series2: 106,
-    series1: 134
-  },
-  {
-    month: "Nov",
-    series2: 88,
-    series1: 110
-  },
-  {
-    month: "Dec",
-    series2: 56,
-    series1: 91
-  }
-];
-
 export default function() {
   const history = useHistory();
   const { symbols } = useParams<{symbols:string}>();
-  const [reportedEPS, setReportedEPS] = useState<any[]>();
+  const [reportedEPS, setReportedEPS] = useState<any[]>([]);
   useEffect(() => {
     (async function() {
       const stocks = symbols.split(",");
       if(stocks.length>0) {
         let earningsArray: any[] = [];
         for(const stock of stocks) {
-          const {data} = await earnings(stock);
-          earningsArray.push(data);
+          const {data: {
+            annualEarnings,
+            symbol
+          }} = await earnings(stock);
+          earningsArray = earningsArray.concat(annualEarnings.map((er: {fiscalDateEnding: any; reportedEPS: any;}) => (
+            {
+              key: symbol,
+              month: er.fiscalDateEnding,
+              value: Number(er.reportedEPS)
+            }
+          )));
         }
         setReportedEPS(earningsArray);
       }
@@ -98,7 +44,7 @@ export default function() {
         ))}
       </ButtonGroup>
 
-      <StepSeries data={data}/>
+      <StepSeries data={reportedEPS}/>
     </div>
   )
 }
--- src/components/StepSeries.tsx
+++ src/components/StepSeries.tsx
@@ -13,24 +13,15 @@ import {
 	Facet,
 	Util
 } from "bizcharts";
-import DataSet from "@antv/data-set";
 
 export default function StepSeries({data}: {data: any[]}) {
-  const ds = new DataSet();
-  const dv = ds.createView().source(data);
-  dv.transform({
-    type: "fold",
-    fields: ["series1", "series2"],
-    key: "key",
-    value: "value"
-  });
   const cols = {
     month: {
       range: [0, 1]
     }
   };
   return (
-    <Chart height={400} appendPadding={[10,0,0,0]} data={dv} scale={cols} autoFit>
+    <Chart height={400} appendPadding={[10,0,0,0]} data={data} scale={cols} autoFit>
       <Legend />
       <Axis name="month" />
       <Axis name="value" />
