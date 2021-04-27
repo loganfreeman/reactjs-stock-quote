a8306138b5662254294ae4aa2addb29d8b4d8d84
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-26 22:25:40 -0600

    favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import './App.css';
 import {Helmet} from "react-helmet";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Search from './components/Search';
 import { takeRight } from 'lodash';
 import { normalize } from './utils/normalize';
 import Stock from "./components/Stock";
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './ErrorBoundary';
+import Grid from '@material-ui/core/Grid';
+
 function App() {
   const [stocks, setStocks] = useState<any[]>([]);
+
+  const [favorites, setFavorites] = useState<any[]>([]);
+
+  useEffect(() => {
+    if(stocks && stocks.length > 0) localStorage.setItem("STOCKS", JSON.stringify(stocks))
+  }, [stocks]);
+
+  useEffect(() => {
+    const stored = localStorage.getItem("STOCKS");
+    if(stored) setFavorites(JSON.parse(stored));
+  }, []);
+
 
   return (
 
       <ErrorBoundary className="App">
-        <Search onChange={setStocks}/>
-        {
-          takeRight(stocks, 3).reverse().map(s => normalize(s)).map(stock => <Stock stock={stock} key={stock.symbol} />)
-        }
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <h1>Enter stock symbol to search</h1>
+            <Search onChange={stocks => setStocks(takeRight(stocks, 3).reverse().map(s => normalize(s)))}/>
+            {
+              stocks.map(stock => <Stock stock={stock} key={stock.symbol} />)
+            }
+        </Grid>
+          <Grid item xs={6}>
+            <h1>My Favorites</h1>
+            {
+              favorites.map(stock => <Stock stock={stock} key={stock.symbol} />)
+            }
+
+          </Grid>
+        </Grid>
         <Helmet>
             <meta charSet="utf-8" />
             <title>Stock Quote</title>
-            <link rel="canonical" href="http://mysite.com/example" />
         </Helmet>
       </ErrorBoundary>
   );
