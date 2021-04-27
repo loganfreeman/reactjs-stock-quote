f9c1ed15d0e7cbf94e077da926a91bb4b0e967bd
Shanhong Cheng <shanhong.cheng@wildworks.com>
2021-04-26 18:42:47 -0600

    helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
+import Search from './components/Search';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <Search />
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Stock Quote</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+      </div>
   );
 }
 
